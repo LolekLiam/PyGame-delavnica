de02be5c830e3c46f49c49cb5955b5e807623ccd
LolekLiam <liamstaric@gmail.com>
2024-05-09 16:43:49 +0200

    make basic gameover and win logic

--- igra1.py
+++ igra1.py
@@ -10,6 +10,7 @@ screen=pygame.display.set_mode((1020,661)) #Najava zaslona
 pygame.display.set_caption("Moja prva igra") #Ime igre
 bg=pygame.image.load("bg.jpg").convert_alpha() #Najava ozadja
 font=pygame.font.SysFont(name="Comic sans",size=32) #Najava fonta xd
+font2=pygame.font.SysFont(name="Comic sans",size=100) #Najava fonta2 xd
 target_pos=(100,100)
 pnt=0
 
@@ -54,7 +55,11 @@ while True:
     screen.blit(text,(20,25))
 
     if pnt<=-5:
-        exit()
+        gmover=font2.render("GAME OVER",True,(255,0,0))
+        screen.blit(gmover,(210,230))
+    if pnt>=10:
+        win=font2.render("YOU WIN",True,(255,0,0))
+        screen.blit(win,(260,230))
 
     pygame.display.update() #Next frame, vedno mora biti na koncu
     clock.tick(60) #Razen clock, ker je pač clock Xd
