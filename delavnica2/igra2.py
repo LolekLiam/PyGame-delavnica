aad9ff008094f4a8e88946f89d537f2cac5dbf01
LolekLiam <liamstaric@gmail.com>
2024-05-13 15:18:37 +0200

    Update igra2.py

--- delavnica2/igra2.py
+++ delavnica2/igra2.py
@@ -28,25 +28,35 @@ e2_x=300
 e2_y=0
 e2_speed=2
 
+blt_x=1
+blt_y=1
+blt_speed=8
+
 while True:
+    mouse_pos=pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            if blt_y<0:
+                blt_x=mouse_pos[0]+15
+                blt_y=rckt_y
     e1_y+=e1_speed
     e2_y+=e2_speed
     if e1_y>800:
         e1_x=random.randint(100,700)
         e1_y=0
+        e1_speed=random.randint(2,5)
     if e2_y>800:
         e2_x=random.randint(100,700)
         e2_y=0
-    mouse_pos=pygame.mouse.get_pos()
+        e2_speed=random.randint(2,5)
     screen.fill("black")
     screen.blit(erckt1,(e1_x,e1_y))
     screen.blit(erckt2,(e2_x,e2_y))
     screen.blit(rckt,(mouse_pos[0],rckt_y))
-    target_pos=(target_pos[0],target_pos[1]+3)
+    pygame.draw.circle(screen,"red",(blt_x,blt_y),2)
 
     pygame.display.update() #Next frame, vedno mora biti na koncu
     clock.tick(60) #Razen clock, ker je pač clock Xd
